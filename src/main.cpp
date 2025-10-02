41f798e9fcb3dca90414c2114ba6c841a15db871
Egakrut3 <157879824+Egakrut3@users.noreply.github.com>
2025-10-02 12:11:56 +0300

    Modify codestyle

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 int main(int const argc, char const *const *const argv) {
     assert(argc > 0); assert(argv); assert(*argv);
 
-    struct Config cur_config = {};
+    struct Config cur_config    = {};
     struct User_error cur_error = {};
 #undef FINAL_CODE
 #define FINAL_CODE                      \
@@ -16,7 +16,7 @@ int main(int const argc, char const *const *const argv) {
 
     MAIN_CHECK_FUNC(set_config, &cur_error, &cur_config, argc, argv);
     if (handle_User_error(&cur_error)) { CLEAR_RESOURCES(); return 0; }
-    if (cur_config.is_help) { CLEAR_RESOURCES(); return 0; }
+    if (cur_config.is_help)            { CLEAR_RESOURCES(); return 0; }
 
     destruct_User_error(&cur_error);
     MAIN_CHECK_FUNC(make_byte_code, &cur_error, cur_config.input_stream, cur_config.output_stream);
